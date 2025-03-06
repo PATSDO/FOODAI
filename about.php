276af7c42a79419ef758fd19ddcf2d556b5b9946
ferrofluidd <ferrofluidd@gmail.com>
2025-03-06 16:01:27 +0800

    Update about.php

--- about.php
+++ about.php
@@ -1,3 +1,5 @@
+<?php include 'db_connection.php'; ?> 
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -39,7 +41,7 @@
                     <i class="bi bi-envelope fs-1 text-primary me-3"></i>
                     <div class="text-start">
                         <h6 style="font-family: Times New Roman" class="text-uppercase mb-1">Email Us</h6>
-                        <span>soulfoodmanila@gmail.com</span>
+                        <span>foodai@gmail.com</span>
                     </div>
                 </div>
             </div>
@@ -52,10 +54,9 @@
             </div>
             <div class="col-lg-4 text-center bg-secondary py-3">
                 <div class="d-inline-flex align-items-center justify-content-center">
-                    <i class="bi bi-phone-vibrate fs-1 text-primary me-3"></i>
                     <div class="text-start">
-                        <h6 style="font-family: Times New Roman" class="text-uppercase mb-1">Call Us</h6>
-                        <span>+012 345 6789</span>
+                        <a href="register.php" class="navbar-brand"><button  style="font-family: Times New Roman" type="button" class="btn btn-primary btn-lg">Register</button></a>
+                        <a href="login.php" class="navbar-brand"><button style="font-family: Times New Roman" type="button" class="btn btn-primary btn-lg">Login</button></a>
                     </div>
                 </div>
             </div>
@@ -163,13 +164,13 @@
    
 
  <!-- Footer Start -->
- <div class="container-fluid bg-img text-secondary" style="margin-top: 135px">
+    <div class="container-fluid bg-img text-secondary" style="margin-top: 90px">
         <div class="container">
             <div class="row gx-5">
-                <div class="col-lg-4 col-md-6 mt-lg-n5">
+                <div class="col-lg-4 col-md-6 mb-lg-n5">
                     <div class="d-flex flex-column align-items-center justify-content-center text-center h-100 bg-primary border-inner p-4">
                         <a href="index.html" class="navbar-brand">
-                            <h1 class="m-0 text-uppercase text-white"><i class="fa fa-birthday-cake fs-1 text-dark me-3"></i>Food AI</h1>
+                            <h1 class="m-0 text-uppercase text-white"></i>Food AI</h1>
                         </a>
                         <p class="mt-3">Discover the best local dining experiences tailored just for you! FAI (Food AI) is an intelligent food recommendation system that uses artificial intelligence to suggest personalized meal options based on your preferences, dietary needs, and real-time availability from nearby fast food restaurants.</p>
                     </div>
@@ -194,7 +195,7 @@
                         <div class="col-lg-4 col-md-12 pt-0 pt-lg-5 mb-5">
                             <h4 class="text-primary text-uppercase mb-4">Quick Links</h4>
                             <div class="d-flex flex-column justify-content-start">
-                                <a class="text-secondary mb-2" href="index.html"><i class="bi bi-arrow-right text-primary me-2"></i>Home</a>
+                            <a class="text-secondary mb-2" href="index.html"><i class="bi bi-arrow-right text-primary me-2"></i>Home</a>
                                 <a class="text-secondary mb-2" href="about.php"><i class="bi bi-arrow-right text-primary me-2"></i>FOODAI</a>
                                 <a class="text-secondary mb-2" href="menu.html"><i class="bi bi-arrow-right text-primary me-2"></i>Jollibee</a>
                                 <a class="text-secondary mb-2" href="menu2.html"><i class="bi bi-arrow-right text-primary me-2"></i>Mcdonalds</a>
@@ -203,8 +204,8 @@
                             </div>
                         </div>
                         <div class="col-lg-4 col-md-12 pt-0 pt-lg-5 mb-5">
-                            <h4 class="text-primary text-uppercase mb-4">Newsletter</h4>
-                            <p>foodai@gmail.com</p>
+                            <h4 class="text-primary text-uppercase mb-4">Email Us</h4>
+                            <p>Amet justo diam dolor rebum lorem sit stet sea justo kasd</p>
                             <form action="">
                                 <div class="input-group">
                                     <input type="text" class="form-control border-white p-3" placeholder="Your Email">
@@ -219,7 +220,7 @@
     </div>
     <div class="container-fluid text-secondary py-4" style="background: #111111;">
         <div class="container text-center">
-            <p class="mb-0">&copy; <a class="text-white border-bottom" href="index.html">Food AI</a>. by Cipriano, Bandal, De Ocampo, Gesmundo, & Pua</a></p>
+            <p class="mb-0">&copy; <a class="text-white border-bottom" href="#">Food AI</a>. by Cipriano, Bandal, De Ocampo, Gesmundo, & Pua</p>
         </div>
     </div>
     <!-- Footer End -->
