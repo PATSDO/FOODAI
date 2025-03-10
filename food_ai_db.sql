514991db2226ac505ec54394ad6aedd0a8a07184
asha <123242887+ashmacaron@users.noreply.github.com>
2025-03-11 00:11:21 +0800

    gagie

--- food_ai_db.sql
+++ food_ai_db.sql
@@ -1,27 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 10, 2025 at 04:57 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.1.25
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `food_ai_db`
---
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `menu`
